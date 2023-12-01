f1c5f1ff6a19669420cce1f75d1982db541fe308
nicheilman <heilman7@msu.edu>
2023-12-01 14:00:36 -0500

    Folder cleanup

---
+++ Script/flow.py
@@ -0,0 +1,185 @@
+from mpi4py import MPI
+from petsc4py import PETSc
+import numpy as np
+import pyvista
+import BC
+from fluids import epsilon, sigma
+#import gmsh
+
+from dolfinx.fem import Constant, Function, FunctionSpace, assemble_scalar, dirichletbc, form, locate_dofs_geometrical
+from dolfinx.fem.petsc import assemble_matrix, assemble_vector, apply_lifting, create_vector, set_bc
+from dolfinx.io import VTXWriter
+from dolfinx.mesh import create_unit_cube
+from dolfinx.plot import vtk_mesh
+from ufl import (FacetNormal, FiniteElement, Identity, TestFunction, TrialFunction, VectorElement,
+                 div, dot, ds, dx, inner, lhs, nabla_grad, rhs, sym)
+
+# Spacetime discretization for meshing
+nx = 10
+mesh = create_unit_cube(MPI.COMM_WORLD, nx, nx, nx)
+t = 0
+T = 10
+num_steps = 500
+dt = T / num_steps
+
+# Define function spaces
+v_cg2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
+s_cg1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+V = FunctionSpace(mesh, v_cg2)
+Q = FunctionSpace(mesh, s_cg1)
+
+# Create the test and trial function
+u = TrialFunction(V)
+v = TestFunction(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
+
+# Boundary Conditions
+wall_dofs = locate_dofs_geometrical(V, BC.walls)
+u_noslip = np.array((0,) * mesh.geometry.dim, dtype=PETSc.ScalarType)
+bc_noslip = dirichletbc(u_noslip, wall_dofs, V)
+
+inflow_dofs = locate_dofs_geometrical(Q, BC.inflow)
+bc_inflow = dirichletbc(PETSc.ScalarType(8), inflow_dofs, Q)
+
+outflow_dofs = locate_dofs_geometrical(Q, BC.outflow)
+bc_outflow = dirichletbc(PETSc.ScalarType(0), outflow_dofs, Q)
+bcu = [bc_noslip]
+bcp = [bc_inflow, bc_outflow]
+
+# Defining Navier-Stokes variables 
+u_n = Function(V)
+u_n.name = "u_n"
+U = 0.5 * (u_n + u)
+n = FacetNormal(mesh)
+f = Constant(mesh, PETSc.ScalarType((0, 0, 0)))
+k = Constant(mesh, PETSc.ScalarType(dt))
+mu = Constant(mesh, PETSc.ScalarType(1))
+rho = Constant(mesh, PETSc.ScalarType(1))
+
+# Define the variational problem for the step 1
+p_n = Function(Q)
+p_n.name = "p_n"
+F1 = rho * dot((u - u_n) / k, v) * dx
+F1 += rho * dot(dot(u_n, nabla_grad(u_n)), v) * dx
+F1 += inner(sigma(U, p_n, mu), epsilon(v)) * dx
+F1 += dot(p_n * n, v) * ds - dot(mu * nabla_grad(U) * n, v) * ds
+F1 -= dot(f, v) * dx
+a1 = form(lhs(F1))
+L1 = form(rhs(F1))
+
+# Create linear system for step 1
+A1 = assemble_matrix(a1, bcs=bcu)
+A1.assemble()
+b1 = create_vector(L1)
+
+# Define variational problem for step 2
+u_ = Function(V)
+a2 = form(dot(nabla_grad(p), nabla_grad(q)) * dx)
+L2 = form(dot(nabla_grad(p_n), nabla_grad(q)) * dx - (rho / k) * div(u_) * q * dx)
+
+# Create linear system for second step
+A2 = assemble_matrix(a2, bcs=bcp)
+A2.assemble()
+b2 = create_vector(L2)
+
+# Define variational problem for step 3
+p_ = Function(Q)
+a3 = form(rho * dot(u, v) * dx)
+L3 = form(rho * dot(u_, v) * dx - k * dot(nabla_grad(p_ - p_n), v) * dx)
+
+# Create linear system for step 3
+A3 = assemble_matrix(a3)
+A3.assemble()
+b3 = create_vector(L3)
+
+# Solver for step 1
+solver1 = PETSc.KSP().create(mesh.comm)
+solver1.setOperators(A1)
+solver1.setType(PETSc.KSP.Type.BCGS)
+pc1 = solver1.getPC()
+pc1.setType(PETSc.PC.Type.HYPRE)
+pc1.setHYPREType("boomeramg")
+
+# Solver for step 2
+solver2 = PETSc.KSP().create(mesh.comm)
+solver2.setOperators(A2)
+solver2.setType(PETSc.KSP.Type.BCGS)
+pc2 = solver2.getPC()
+pc2.setType(PETSc.PC.Type.HYPRE)
+pc2.setHYPREType("boomeramg")
+
+# Solver for step 3
+solver3 = PETSc.KSP().create(mesh.comm)
+solver3.setOperators(A3)
+solver3.setType(PETSc.KSP.Type.CG)
+pc3 = solver3.getPC()
+pc3.setType(PETSc.PC.Type.SOR)
+
+
+# I/O and Pathing
+from pathlib import Path
+folder = Path("results")
+folder.mkdir(exist_ok=True, parents=True)
+vtx_mesh = VTXWriter(mesh.comm, folder / "mesh.bp", mesh, engine="BP4")
+vtx_u = VTXWriter(mesh.comm, folder / "u.bp", u_n, engine="BP4")
+vtx_p = VTXWriter(mesh.comm, folder / "p.bp", p_n, engine="BP4")
+vtx_mesh.write(t)
+vtx_u.write(t)
+vtx_p.write(t)
+
+
+# Solver Loop 
+for i in range(num_steps):
+    # Update current time step
+    t += dt
+
+    # Step 1: Tentative veolcity step
+    with b1.localForm() as loc_1:
+        loc_1.set(0)
+    assemble_vector(b1, L1)
+    apply_lifting(b1, [a1], [bcu])
+    b1.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b1, bcu)
+    solver1.solve(b1, u_.vector)
+    u_.x.scatter_forward()
+
+    # Step 2: Pressure corrrection step
+    with b2.localForm() as loc_2:
+        loc_2.set(0)
+    assemble_vector(b2, L2)
+    apply_lifting(b2, [a2], [bcp])
+    b2.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b2, bcp)
+    solver2.solve(b2, p_.vector)
+    p_.x.scatter_forward()
+
+    # Step 3: Velocity correction step
+    with b3.localForm() as loc_3:
+        loc_3.set(0)
+    assemble_vector(b3, L3)
+    b3.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    solver3.solve(b3, u_.vector)
+    u_.x.scatter_forward()
+
+    # Update variable with solution from this time step
+    u_n.x.array[:] = u_.x.array[:]
+    p_n.x.array[:] = p_.x.array[:]
+
+# Write solutions to file
+vtx_u.write(t)
+vtx_p.write(t)
+
+
+# Close xmdf file
+vtx_u.close()
+vtx_p.close()
+b1.destroy()
+b2.destroy()
+b3.destroy()
+solver1.destroy()
+solver2.destroy()
+solver3.destroy()
+
+
+
---
+++ Script/fluids.py
@@ -0,0 +1,10 @@
+from ufl import (Identity, div, dot, ds, dx, inner, lhs, nabla_grad, rhs, sym)
+
+# Define strain-rate tensor
+def epsilon(u):
+    return sym(nabla_grad(u))
+
+# Define stress tensor
+def sigma(u, p, den):
+    return 2 * den * epsilon(u) - p * Identity(len(u))
+
