da791769664f9b3c045158cadc49ab419bda9081
nicheilman <heilman7@msu.edu>
2023-11-30 20:52:08 -0500

    Reformat into simple 3D and start FEM basics

--- flow.py
+++ flow.py
@@ -2,21 +2,38 @@ from mpi4py import MPI
 from petsc4py import PETSc
 import numpy as np
 import pyvista
+#import gmsh
 
 from dolfinx.fem import Constant, Function, FunctionSpace, assemble_scalar, dirichletbc, form, locate_dofs_geometrical
 from dolfinx.fem.petsc import assemble_matrix, assemble_vector, apply_lifting, create_vector, set_bc
 from dolfinx.io import VTXWriter
-from dolfinx.mesh import create_unit_square
+from dolfinx.mesh import create_unit_cube
 from dolfinx.plot import vtk_mesh
 from ufl import (FacetNormal, FiniteElement, Identity, TestFunction, TrialFunction, VectorElement,
                  div, dot, ds, dx, inner, lhs, nabla_grad, rhs, sym)
 
-mesh = create_unit_square(MPI.COMM_WORLD, 10, 10)
+# Spacetime discretization for meshing
+nx = 10
+mesh = create_unit_cube(MPI.COMM_WORLD, 10, 10, 10)
 t = 0
 T = 10
 num_steps = 500
 dt = T / num_steps
 
+# Define function spaces
+v_cg2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
+s_cg1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+V = FunctionSpace(mesh, v_cg2)
+Q = FunctionSpace(mesh, s_cg1)
+
+# Create the test and trial function
+u = TrialFunction(V)
+v = TestFunction(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
+
+
+# I/O and Pathing
 from pathlib import Path
 folder = Path("results")
 folder.mkdir(exist_ok=True, parents=True)
